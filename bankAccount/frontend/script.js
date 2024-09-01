4270133d9f7e024a311a4e92ac0a542b73d06196
prostodiva <rkattsyna@mail.ru>
2024-09-01 10:31:08 -0700

    made the open API work. Bank Account project

--- bankAccount/frontend/script.js
+++ bankAccount/frontend/script.js
@@ -9,14 +9,38 @@ const downpaymentElement = document.querySelector(".downpayment");
 const interestRateElement = document.querySelector(".interest_rate");
 const durationYearsElement = document.querySelector(".duration_years");
 const monthlyPaymentsElement = document.querySelector(".monthly_payment");
+const annualPaymentElement = document.querySelector(".annual_payment");
 const errorElement = document.querySelector(".error");
+
+const totalElement = document.querySelector(".total");
+const mortgageElement = document.querySelector(".mortgage");
+const propetryTaxElement = document.querySelector(".property_tax");
+
+const totalElementTwo = document.querySelector(".totalTwo");
+const mortgageElementTwo = document.querySelector(".mortgageTwo");
+const totalInterestPaidElement = document.querySelector(".total_interest_paid");
+const homeInsuranceElement = document.querySelector(".home_insurance");
+
 
 // Define Dollar and Percent symbols
 const Dollar = '$';
 const Percent = '%';
 
+
 // Add event listener to the submit button
 document.getElementById('submit_button').addEventListener('click', checkMortgageRate);
+//change the color of the button
+const buttonElement = document.getElementById('submit_button');
+buttonElement.addEventListener("mouseover", 
+    (event) => {
+        event.target.style.backgroundColor="rgb(225, 214, 150)";
+});
+//mouseout - color out
+buttonElement.addEventListener("mouseout",
+    (event) => {
+        event.target.style.backgroundColor="";
+    });
+
 
 /**
  * Function to fetch mortgage data from API
@@ -67,15 +91,43 @@ NaN is short for "Not-a-Number". The isNaN() method returns true if a value is N
 
         // Parse JSON response
         var data = await response.json();
-        //fixes the object Object$ problem
-        alert(JSON.stringify(data));
+        //alert window to chech if json return an info
+        //alert(JSON.stringify(data));
 
-        //Update the UI with the response data
-        interestRateElement.innerText = data.interest_rate ? data.interest_rate + Percent : 'N/A';
-        homeValueElement.innerText = data.home_value ? data.home_value + Dollar : 'N/A';
-        downpaymentElement.innerText = data.downpayment ? data.downpayment + Dollar : 'N/A';
-        durationYearsElement.innerText = data.duration_years ? data.duration_years : 'N/A';
-        monthlyPaymentsElement.innerText = data.monthly_payment ? data.monthly_payment + Dollar : 'N/A';
+         //Update the UI with the response data. 
+        totalElement.innerHTML = Dollar + data.monthly_payment.total;
+        mortgageElement.innerHTML = Dollar + data.monthly_payment.mortgage;
+
+        totalElementTwo.innerHTML = Dollar + data.annual_payment.total;
+        mortgageElementTwo.innerHTML = Dollar + data.annual_payment.mortgage;
+        homeInsuranceElement.innerHTML = Dollar + data.annual_payment.home_insurance;
+        totalInterestPaidElement.innerText = Dollar + data.total_interest_paid;
+/**
+ * 
+ * the JSON object that you want to access you need to do it similarly to member variables in a class in C++.
+ *  For example, if you want to get the property tax number from the data object you write it like this:
+    data.monthly_payment.property.tax
+
+         * {
+         * "monthly_payment":
+         * {"total":1033,
+         * "mortgage":1033,
+         * "property_tax":0,
+         * "hoa":0,
+         * "annual_home_ins":0
+         * },
+         * 
+         * "annual_payment":{
+         * "total":12394,
+         * "mortgage":12394,
+         * "property_tax":0,
+         * "hoa":0,
+         * "home_insurance":0
+         * },
+         * "total_interest_paid":141809
+         * }
+         */
+
 
     } catch (error) {
         console.error('Fetch error:', error);
